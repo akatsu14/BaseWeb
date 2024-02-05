3ffdf7d88d0c457d067e28eee6fbfd4684bfd85e
akatsu14 <luong13166@gmail.com>
2024-02-05 20:25:16 +0700

    mybook

--- client/src/commons/constants.js
+++ client/src/commons/constants.js
@@ -1,5 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://my-book-aka-66b47c12c5b3.herokuapp.com/api";
+    : "http://localhost:5000/api";
+    // :"https://my-book-aka-66b47c12c5b3.herokuapp.com/api";
 export const LOCAL_STORAGE_TOKEN_NAME = "readingbook";
