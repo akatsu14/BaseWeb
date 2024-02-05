e65a5b87ece3eacefe8f430b15f39b4e0d406aec
akatsu14 <luong13166@gmail.com>
2024-02-05 11:09:08 +0700

    webbase

--- client/src/commons/constants.js
+++ client/src/commons/constants.js
@@ -1,3 +1,5 @@
 export const apiUrl =
-  process.env.NODE_ENV !== "production" ? "http://localhost:5000/api" : "https://akat-book-0cd555a44d5b.herokuapp.com/api";
-export const LOCAL_STORAGE_TOKEN_NAME="readingbook"; 
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:5000/api"
+    : "https://my-book-aka-66b47c12c5b3.herokuapp.com/api";
+export const LOCAL_STORAGE_TOKEN_NAME = "readingbook";
