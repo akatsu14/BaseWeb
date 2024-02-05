3443e2e2b3bcd278daa37d75b4bbf660e5cbd2eb
akatsu14 <luong13166@gmail.com>
2024-02-05 10:05:32 +0700

    make it better

--- client/src/commons/constants.js
+++ client/src/commons/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl =
-  process.env.NODE_ENV !== "production" ? "http://localhost:5000/api" : "https://morning-peak-66706-eb3fc6ae5c46.herokuapp.com/api";
-export const LOCAL_STORAGE_TOKEN_NAME="readingbook";
+  process.env.NODE_ENV !== "production" ? "http://localhost:5000/api" : "https://akat-book-0cd555a44d5b.herokuapp.com/api";
+export const LOCAL_STORAGE_TOKEN_NAME="readingbook"; 
